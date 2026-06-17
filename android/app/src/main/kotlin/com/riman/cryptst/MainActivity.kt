5923f166a169203de58a37768a66d7a0b57db29f
Heroeen90 <124597053+Heroeen90@users.noreply.github.com>
2026-06-18 02:47:58 +0300

    Update MainActivity.

--- android/app/src/main/kotlin/com/riman/cryptst/MainActivity.kt
+++ android/app/src/main/kotlin/com/riman/cryptst/MainActivity.kt
@@ -3,5 +3,4 @@ package com.riman.cryptst
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    // النواة مهيأة تلقائياً لاستقبال قنوات الاتصال والتشفير العتادي
 }
